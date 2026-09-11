b9c3531281c12984f17959e08ffa23554067b73c
Ravale Shubham <ravaleshubham9@gmail.com>
2026-09-11 11:56:57 +0530

    update api url to render backend

--- frontend/app/api.js
+++ frontend/app/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://enterprise-knowledge-copilot-la27.onrender.com";
 
 // 1. Login Function (टोकन सेव करने के लिए)
 export async function loginUser(username, password) {
