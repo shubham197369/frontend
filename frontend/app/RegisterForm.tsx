0586045867a9d1ac4f0e62868f1f55663b9b4408
Ravale Shubham <ravaleshubham9@gmail.com>
2026-09-12 11:01:14 +0530

    fix: update register url to render backend

--- frontend/app/RegisterForm.tsx
+++ frontend/app/RegisterForm.tsx
@@ -21,7 +21,7 @@ export default function RegisterForm({ onRegisterSuccess, onSwitchToLogin }: Reg
     setSuccess("");
 
     try {
-      const response = await fetch("http://localhost:8000/register", {
+     const response = await fetch("https://enterprise-knowledge-copilot-la27.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
